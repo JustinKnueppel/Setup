7db2e5d5d6078870d64359e1633fb6d8d5b75ea5
Justin Knueppel <justinknueppel@gmail.com>
2019-05-16 01:19:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Setup arch-based linux system
+
+```./setup.sh```
+
---
+++ setup.sh
@@ -0,0 +1,84 @@
+#!/usr/bin bash
+
+# Get directory and cd to home
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ~
+
+# Fix file structure
+rm -rf Videos/ Templates/ Music/ Public/
+
+# Update system and correct clock
+sudo pacman -Syu
+timedatectl set-local-rtc 1 --adjust-system-clock
+
+# Git
+sudo pacman -S gvim yay git
+echo "Enter your email"
+read email
+git config --global user.email "\"${email}\""
+read name
+git config --global user.name "\"${name}\""
+
+# Vim
+yay -S vundle
+git clone git@github.com:JustinKnueppel/Vim-RC.git
+mv Vim-RC/.vimrc ~/.vimrc
+rm -rf Vim-RC
+vim -c VundleUpdate -c quitall
+
+echo "export VISUAL=vim" >> ~/.bashrc
+echo "export EDITOR=\$VISUAL" >> ~/.bashrc
+
+# Git utilities
+mkdir Coding
+git clone git@github.com:JustinKnueppel/Git-Utilities.git ~/Coding/Git-Utilities
+
+"alias ug=~/Coding/Git-Utilities/UpdateGitRepos.sh" >> ~/.bashrc
+"alias ugl=~/Coding/Git-Utilities/UpdateGitList.sh" >> ~/.bashrc
+~/Coding/Git-Utilities/UpdateGitList.sh
+
+# Python environment
+pip install --upgrade --user pip
+mkdir ~/Backups
+pip freeze > ~/Backups/baserequirements.txt
+pip install --user virtualenv
+mkdir venv
+cd venv
+virtualenv general
+cd
+echo "alias general='source ~/venv/general/bin/activate'" >> ~/.bashrc
+activate ~/venv/general/bin/activate
+pip install -r "${DIR}/requirements.txt"
+deactivate
+
+# Node
+yay -S nvm
+nvm install-latest-npm
+source /usr/share/nvm/init-nvm.sh
+npm i -g eslint typescript
+
+# Redshift
+sudo pacman -S redshift
+mkdir ~/.config/redshift
+cat "${DIR}/redshift.conf" > ~/.config/redshift/redshift.conf
+sudo systemctl enable redshift
+sudo systemctl start redshift
+
+# VS Code
+yay -S code
+
+# SSH
+echo "add_key()" >> ~/.bashrc
+echo "{" >> ~/.bashrc
+echo "  if [ -z "$SSH_AUTH_SOCK" ] ; then" >> ~/.bashrc
+echo "      eval `ssh-agent -s`" >> ~/.bashrc
+echo "  fi" >> ~/.bashrc
+echo "  ssh-add" >> ~/.bashrc
+echo "}" >> ~/.bashrc
+
+echo "alias ssh-add=add_key" >> ~/.bashrc
+
+ssh-keygen -o -a 100 -t ed25519
+
+source ~/.bashrc
+
