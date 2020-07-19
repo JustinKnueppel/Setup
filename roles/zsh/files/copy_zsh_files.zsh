efd6a91f8734e689d8b6393fcd8f7f487f89e3d0
Justin <justinknueppel@gmail.com>
2020-07-18 20:31:25 -0400

    fix symlinks

--- roles/zsh/files/copy_zsh_files.zsh
+++ roles/zsh/files/copy_zsh_files.zsh
@@ -2,6 +2,6 @@
 
 for rcfile in $(cd "$HOME"/.zprezto/runcoms/ && ls | grep "z"); do
   if [ ! -e "$HOME/.${rcfile}" ]; then
-    ln -s "$rcfile" "$HOME/.${rcfile}"
+    ln -s "$HOME/.zprezto/runcoms/$rcfile" "$HOME/.${rcfile}"
   fi
 done
